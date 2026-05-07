55e73ca0ba454e42eefc866b9817c0e155a0c285
gabriel caillon <gabrielcaillon30@gmail.com>
2026-05-07 11:09:54 +0200

    zsqet

--- script.js
+++ script.js
@@ -92,11 +92,24 @@ function createPuzzle(board, difficulty = 40) {
 }
 
 
-let fullGrid = generateSudoku();
-const puzzle = createPuzzle(fullGrid, 45); 
+// Puzzle de test pré-défini pour déboguer l'affichage
+const puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
  
 window.onload = function () {
     const grid = document.querySelector(".grid");
+    
+    console.log("Grille:", grid);
+    console.log("Puzzle:", puzzle);
 
     for (let i = 0; i < 9; i++) { 
         for (let j = 0; j < 9; j++) { 
